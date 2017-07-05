8a6ed9b679b349207eebcd63b931d2e1baf83523
Rdyx <tiranico.pro@gmail.com>
2017-07-05 14:10:26 +0200

    7eme_etape

--- functions/functions.js
+++ functions/functions.js
@@ -1,4 +1,4 @@
-function fonction(param1, param2, param3){
+/*function fonction(param1, param2, param3){
 
 	return alert("Bonjour " + param1 + " " + param2 + ". Tu as " + param3 + " ans.");
 }
@@ -6,4 +6,24 @@ function fonction(param1, param2, param3){
 var nom = prompt("Quel est votre nom?");
 var prenom = prompt("Quel est votre prénom?");
 var age = prompt("Quel est votre âge?");
-fonction(nom, prenom, age);
+fonction(nom, prenom, age);*/
+
+
+function ageGenre(param1, param2){
+	if (param1 < 18 && param2 == "Homme" || param1 < 18 && param2 == "Femme")
+	{
+		return alert("Vous êtes un/une " + param2 + " et vous êtes mineur");
+	}
+	else if (param1 >= 18 && param2 == "Homme" || param2 == "Femme")
+	{
+		return alert("Vous êtes un/une " + param2 + " et vous êtes majeur")
+	}
+	else
+	{
+		return alert("Ben vous êtes quoi ?!")
+	}
+}
+
+var age = Number(prompt("Quel est votre âge ?"));
+var genre = prompt("Et votre genre ?");
+ageGenre(age, genre);
