48974562a9a59b36989d457453845df02e8dc573
Rdyx <tiranico.pro@gmail.com>
2017-07-05 12:11:37 +0200

    4eme

--- functions/functions.js
+++ functions/functions.js
@@ -1,6 +1,18 @@
 function fonction(param1, param2){
-
+	if(param1>param2){
+		return alert(param1 + " est supérieur à "+ param2);
+	}
+	else if(param1<param2){
+		return alert(param1 + " est inférieur à "+ param2);
+	}
+	else {
+		return alert(param1 + " est égal à "+ param2);
+	}
 	return alert("Param1 : " + param1 + " " + param2);
 }
 
-fonction("Une chaine ", "2Chaine");
+
+var chiffre1 = Number(prompt("1er chiffre"));
+var chiffre2 = Number(prompt("2eme chiffre"));
+
+fonction(chiffre1, chiffre2);
