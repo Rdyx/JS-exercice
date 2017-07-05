44a68ea4af925a4ee8bbaecbf5c55bf06d6660d4
Rdyx <tiranico.pro@gmail.com>
2017-07-05 12:33:42 +0200

    exo1-2-fini

--- array/app.js
+++ array/app.js
@@ -3,4 +3,5 @@ var array = ["Pomme","Cerise","Tomate","Citron", ["Orange","Poivron"]];
 array.push("Courgette");
 array[4].push("Citron");
 array.splice(1,1);
+array.splice(2, 0, "Poire");
 console.log(array);
