703dd1039cdf225edf8f6bdff8cf22aa5199e43b
Rdyx <tiranico.pro@gmail.com>
2017-07-05 13:31:51 +0200

    5eme_etape

--- functions/functions.js
+++ functions/functions.js
@@ -1,18 +1,7 @@
 function fonction(param1, param2){
-	if(param1>param2){
-		return alert(param1 + " est supérieur à "+ param2);
-	}
-	else if(param1<param2){
-		return alert(param1 + " est inférieur à "+ param2);
-	}
-	else {
-		return alert(param1 + " est égal à "+ param2);
-	}
-	return alert("Param1 : " + param1 + " " + param2);
+
+	return alert(param1 + " " + param2);
 }
 
 
-var chiffre1 = Number(prompt("1er chiffre"));
-var chiffre2 = Number(prompt("2eme chiffre"));
-
-fonction(chiffre1, chiffre2);
+fonction(3, "Concaténation du chiffre");
